d8f552f647b1dc5f212b99b3ab86afda952f2a82
Lucas Silva <silva.los@outlook.com>
2024-09-07 20:51:51 -0300

    Implemented possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,6 +32,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		
 		return (ChessPiece) capturedPiece;
@@ -44,14 +45,22 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 	
-	private void validateSourcePosition(Position position) {
-		if (!board.thereIsAPiece(position)) {
+	private void validateSourcePosition(Position source) {
+		if (!board.thereIsAPiece(source)) {
 			throw new ChessException("There is no piece on source position");
 		}
-		if (!board.piece(position).isThereAnyPossibleMove()) {
+		if (!board.piece(source).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for chones piece");
 
 		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to the chosen target");
+		}
+		
 	}
 	
 	
