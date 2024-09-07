ec5369f2c6bf9efdac08a63f857b26f6ca0052e0
Lucas Silva <silva.los@outlook.com>
2024-09-07 17:49:55 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -64,6 +64,27 @@ public class Board {
 	}
 	
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
+		
+		
+	}
+	
+	
+	
+	
 	public boolean positionExists(int row, int column) {
 		return row >=0 && row < rows && column >= 0 && column < columns;
 	}
@@ -80,6 +101,8 @@ public class Board {
 		
 		return piece(position) != null;
 	}
+	
+	
 	
 	
 }
