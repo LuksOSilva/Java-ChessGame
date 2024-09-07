252614fed3527accccea49f7c60e8b33db7d9c70
Lucas Silva <silva.los@outlook.com>
2024-09-07 16:57:37 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,28 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+	
+	private Color color;
+
+	
+	//CONSTRUCTORS
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+	
+	//GETS & SETS
+
+	public Color getColor() {
+		return color;
+	}
+	
+	
+	
+
+}
