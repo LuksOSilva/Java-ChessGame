2c79b337836471f59622b2d08e8e600aeb41037f
Lucas Silva <silva.los@outlook.com>
2024-09-07 16:35:49 -0300

    New class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,47 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	
+	//CONSTRUCTORS
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	
+	//GETS & SETS
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	
+	//METHODS
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+	
+	
+	
+}
